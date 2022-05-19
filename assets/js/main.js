fe6a1ee31fc05b646623a99aeba30f5cc36e08a9
Hellimateas Chaves <91029460+Hellimateas@users.noreply.github.com>
2022-05-18 22:09:14 -0300

    botaos de passar os projetos

--- assets/js/main.js
+++ assets/js/main.js
@@ -83,15 +83,15 @@ tabs.forEach(tab =>{
 /*==================== PORTFOLIO SWIPER  ====================*/
 let swiper = new Swiper(".mySwiper", {
     cssMode: true,
+    loop:true,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
     pagination: {
       el: ".swiper-pagination",
+      clickable: true,
     },
-    mousewheel: true,
-    keyboard: true,
   });
 
 /*==================== TESTIMONIAL ====================*/
